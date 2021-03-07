75d25730905518b3ee45b90d5601b5590b291068
Adrian-Mihai Onofrei <woozydeveloper@gmail.com>
2021-03-07 12:27:55 +0200

    made some cars to go to the right side

--- Scripts/OtherCar.cs
+++ Scripts/OtherCar.cs
@@ -7,12 +7,14 @@ public class OtherCar : MonoBehaviour
     private float speedMovement;
     private CarMovement playerCar;
     private Rigidbody currentCar;
+    int num;
     void Start()
     {
         currentCar = GetComponent<Rigidbody>();
         playerCar = FindObjectOfType<CarMovement>();
 
         speedMovement = Random.Range(0, 10f);
+        num = Random.Range(1, 20);
     }
 
 
@@ -20,6 +22,13 @@ public class OtherCar : MonoBehaviour
     {
         //TODO: add a method to spawn the cars
         currentCar.velocity = new Vector3(0f, 0f, speedMovement);
+        if(num == 5 && currentCar.transform.position.x < 5.5f)
+        {
+            Vector3 newPosition = new Vector3(currentCar.transform.position.x + 1f,
+                                          currentCar.transform.position.y,
+                                          currentCar.transform.position.z);
+            currentCar.transform.position = Vector3.MoveTowards(transform.position, newPosition, 5 * Time.deltaTime);
+        }
         if (playerCar.transform.position.z - this.transform.position.z >= 20f)
         {
             Debug.Log("---CAR DESTROYED---");
