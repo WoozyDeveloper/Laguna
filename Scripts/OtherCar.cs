40a60421c64d3ed64109d0b9404a4af3cc843ab1
Adrian-Mihai Onofrei <woozydeveloper@gmail.com>
2021-03-27 01:36:36 +0200

    Cars switch lane random

--- Scripts/OtherCar.cs
+++ Scripts/OtherCar.cs
@@ -5,31 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class OtherCar : MonoBehaviour
 {
+    #region variables
     Scene currentScene;
     bool changedToLeft = false, changedToRight = false;//used in change lane to see if the car already switched a lane or not
     public float[] oxPositions = new float[4];//main positions on ox for the cars
     private float speedMovement;//movement of the cars with random value (see Start())
     private CarMovement playerCar;
     private Rigidbody currentCar;
-    int direction;
+    private int wannaChangeTheLane;
+    #endregion
     void Start()
     {
+        #region Basic Inits
         currentCar = GetComponent<Rigidbody>();
         playerCar = FindObjectOfType<CarMovement>();
 
         speedMovement = Random.Range(0, 10f);
-        direction = Random.Range(0, 2);
 
         if (transform.position.x == 2f || transform.position.x == -6f)
             changedToRight = true;//can go only to the right lane
         else if (transform.position.x == 6f || transform.position.x == -2f)
             changedToLeft = true;//can go only to the left lane
+        #endregion
+        wannaChangeTheLane = Random.Range(-1, 4);//for P = 1/5
     }
 
     void Update()
     {
         currentCar.velocity = new Vector3(0, 0, speedMovement);
-        if(currentCar.transform.position.z - playerCar.transform.position.z <= 20f)
+        if(wannaChangeTheLane < 0 && currentCar.transform.position.z - playerCar.transform.position.z <= 20f)
             ChangeLane();
 
         if (playerCar.transform.position.z - currentCar.transform.position.z >= 20f)
@@ -76,6 +80,5 @@ public class OtherCar : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
             playerCar.Death();
-
     }
 }
