347ccbb2cacb2e418ef3ef528f8b9eb36b448298
Adrian-Mihai Onofrei <woozydeveloper@gmail.com>
2021-04-16 16:24:40 +0300

    wheel rot

--- Scripts/WheelScript.cs
+++ Scripts/WheelScript.cs
@@ -4,19 +4,18 @@ using UnityEngine;
 
 public class WheelScript : MonoBehaviour
 {
-    GameObject wheel;
-    public float rotationSpeed;
+    public GameObject wheel;
+    private float rotationSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-        rotationSpeed = 50;
-        wheel = GetComponent<GameObject>();
+        rotationSpeed = -100;
     }
 
     // Update is called once per frame
     void Update()
     {
-        wheel.transform.Rotate(rotationSpeed * Time.deltaTime, 0f, 0f);
+        wheel.transform.Rotate(0f, 0f, rotationSpeed * Time.deltaTime);
     }
 }
