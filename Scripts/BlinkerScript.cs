1b3baf6fbc244b6bb43ec939c776f508298be587
Adrian-Mihai Onofrei <woozydeveloper@gmail.com>
2021-04-26 22:58:46 +0300

    Blinkers blink

--- Scripts/BlinkerScript.cs
+++ Scripts/BlinkerScript.cs
@@ -4,37 +4,34 @@ using UnityEngine;
 
 public class BlinkerScript : MonoBehaviour
 {
+    float countdown;
     private bool startBlink;
+    bool change;
     Renderer blinkerRenderer;
     // Start is called before the first frame update
     void Start()
     {
         blinkerRenderer = GetComponent<Renderer>();
         startBlink = true;
+        change = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(startBlink == true)
+        if (countdown > 0)
         {
-            blinkerRenderer.material.SetColor("active_blink", Color.yellow);
-            StartCoroutine(ExampleCoroutine());
-            blinkerRenderer.material.SetColor("_Color", Color.black);
-            //StartCoroutine(ExampleCoroutine());
+            countdown -= Time.deltaTime;
         }
-    }
-
-    IEnumerator ExampleCoroutine()
-    {
-        //Print the time of when the function is first called.
-        Debug.Log("Started Coroutine at timestamp : " + Time.time);
-
-        //yield on a new YieldInstruction that waits for 3 seconds.
-        yield return new WaitForSeconds(1);
-
-        //After we have waited 5 seconds print the time again.
-        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+        else
+        {
+            countdown = .2f;
+            if (change == false)
+                blinkerRenderer.material.SetColor("_Color", Color.yellow);
+            else
+                blinkerRenderer.material.SetColor("_Color", Color.black);
+        }
+        change = !change;
     }
 
     void changeState()
