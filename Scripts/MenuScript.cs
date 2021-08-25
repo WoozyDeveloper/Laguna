97a10577695dd478c6515dbbeae61bd9e57450b1
Adrian-Mihai Onofrei <woozydeveloper@gmail.com>
2021-08-26 01:37:16 +0300

    intro animation + menu

--- Scripts/MenuScript.cs
+++ Scripts/MenuScript.cs
@@ -1,37 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuScript : MonoBehaviour
 {
-    //press on start button
-    private void StartButton()
-    {
-        //activate the animation of the camera and move it to the garage
-        
-    }
-
-    //press on options button
-    private void OptionsButton()
-    {
-        //create an options scene (or credits instead)
-    }
-
-    //press on exit button
-    private void ExitButton()
-    {
-        Application.Quit();
-    }
+    [SerializeField] private Button start,options,exit;
+    private const float lastCameraPositionOZ = -29.82f;//last position of the camera on OZ after the intro animation
+    private bool clicked;//see if the user clicked on a button (any button)
 
     // Start is called before the first frame update
     private void Start()
     {
+        ShowButtons(false);//hide the buttons bcs of the intro animation
+        clicked = false;//set the default value
+    }
+
+    
+    //press on start button
+    public void StartButton()
+    {
+        //activate the animation of the camera and move it to the garage
+        //see animator
+        
+        //hide the buttons
+        ShowButtons(false);
+        clicked = true;
         
     }
+
+    //press on options button
+    public void OptionsButton()
+    {
+        //create an options scene (or credits instead)
+
+        //hide the buttons
+        ShowButtons(false);
+        clicked = true;
+    }
+
+    //press on exit button
+    public void ExitButton() => Application.Quit();
 
     // Update is called once per frame
     private void Update()
     {
-        
+        if(transform.gameObject.transform.position.z == lastCameraPositionOZ)
+        {
+            if(clicked == true)//if the user clicked a button exit the function
+                return;
+            if(start.gameObject.active == false)//check if the buttons are already displayed
+            {
+                Debug.Log("LE PUN");
+                ShowButtons(true);
+            }
+        }
+    }
+
+    //functions made to set the visibility for all the buttons (start, options and exit)
+    private void ShowButtons(bool x)
+    {
+        start.gameObject.SetActive(x);
+        options.gameObject.SetActive(x);
+        exit.gameObject.SetActive(x);
     }
 }
