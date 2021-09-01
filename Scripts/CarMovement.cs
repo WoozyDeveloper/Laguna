69bf16ef62ef9b62e9df5293fe2775cc6ac6c22b
Adrian-Mihai Onofrei <woozydeveloper@gmail.com>
2021-09-01 21:45:10 +0300

    acc + brake with hold

--- Scripts/CarMovement.cs
+++ Scripts/CarMovement.cs
@@ -9,8 +9,8 @@ public class CarMovement : MonoBehaviour
     //right, left OX
     //up, down OY
     //front, back OZ
-    public Button acceleration;
-    private bool buttonPressed;
+    public Button acceleration, brake;
+    private bool accelerationPressed, brakePressed;
     public bool freezeGame;
     public GameObject redArrow, speedometer;
     public float turnSpeed = 10f;
@@ -27,13 +27,15 @@ public class CarMovement : MonoBehaviour
 
         //hide the UI for the game
         acceleration.gameObject.SetActive(false);
+        brake.gameObject.SetActive(false);
         redArrow.gameObject.SetActive(false);
         speedometer.gameObject.SetActive(false);
 
 
         freezeGame = true;//freeze the game until you start it
         Application.targetFrameRate = 160;
-        buttonPressed = false;//the acceleration
+        accelerationPressed = false;//the acceleration
+        brakePressed = false;//the brake
 
         car = GetComponent<Rigidbody>();//current car
 
@@ -51,9 +53,12 @@ public class CarMovement : MonoBehaviour
     {
         if(!freezeGame)
         {
+            if(transform.position.y == .0f)
+                car.constraints = RigidbodyConstraints.FreezePositionY;
             if(!acceleration.IsActive())
             {
                 acceleration.gameObject.SetActive(true);
+                brake.gameObject.SetActive(true);
                 redArrow.gameObject.SetActive(true);
                 speedometer.gameObject.SetActive(true);
             }
@@ -70,14 +75,8 @@ public class CarMovement : MonoBehaviour
             //touch/mouse position
             Vector2 mousePos = Input.mousePosition;//mouse pos(x,y)
 
-            if (Input.GetMouseButton(0) && mousePos.x > Screen.width / 2 && mousePos.y < Screen.height / 10)//if you press the acceleration for bottom right
-            {
-                //TODO: add sound
-                buttonPressed = true;
-            }
-
             //for RIGHT direction
-            if (Input.GetKey(KeyCode.D) || (mousePos.x > Screen.width / 2 && Input.GetMouseButton(0) && buttonPressed == false))
+            if (Input.GetKey(KeyCode.D))
             {
                 #region movement of the car to right
 
@@ -95,7 +94,7 @@ public class CarMovement : MonoBehaviour
                 #endregion
             }
             //for LEFT direction
-            else if (Input.GetKey(KeyCode.A) || (Input.GetMouseButton(0) && mousePos.x < Screen.width / 2))
+            else if (Input.GetKey(KeyCode.A))
             {
                 #region movement of the car to left
 
@@ -114,40 +113,70 @@ public class CarMovement : MonoBehaviour
             //when there's no action
             else
             {
-                float descendingTurn = car.velocity.x;
-                bool sign = descendingTurn > 0;
-                car.velocity = new Vector3(0, 0, carSpeed);
+                car.velocity = new Vector3(car.velocity.x, 0, carSpeed);
 
                 transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, 0), 8 * Time.deltaTime);
                 currentTurnSpeed = 0f;
             }
 
             //boost / nitro / NOS / TOKYO DRIFT / ETC
-            if (Input.GetKey(KeyCode.LeftShift) || buttonPressed == true)//! ! ! TODO: add a button on the screen
+            if (Input.GetKey(KeyCode.LeftShift) || accelerationPressed == true)//! ! ! TODO: add a button on the screen
             {
                 if (carSpeed <= topSpeed)//100 is the max speed
                     carSpeed += 0.2f;//acceleration
+                //tilt
                 this.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(-10, this.transform.rotation.y, this.transform.rotation.z), 10 * Time.deltaTime);
+                //scale of the pedal
                 if (acceleration.transform.localScale.y >= 0.8f)
                     acceleration.transform.localScale = new Vector2(acceleration.transform.localScale.x, acceleration.transform.localScale.y - 0.01f);
             }
-            else//hoo prrr easy easy
+            //brake / stop / hooo prrrr prrr hard
+            else if(Input.GetKey(KeyCode.Space) || brakePressed == true)
             {
+                if(carSpeed >= 12f)
+                carSpeed -= .5f;
+
+                brake.transform.localScale = new Vector2(brake.transform.localScale.x, 0.5f);
+            }
+            else//hoo prrr easy easy stam asa cu mainile la ceafa ce sa facem si noi daca nu apesi pe butoane
+            {
+                //scale of the pedal
                 if (acceleration.transform.localScale.y <= 1f)
                     acceleration.transform.localScale = new Vector2(acceleration.transform.localScale.x, acceleration.transform.localScale.y + 0.02f);
 
                 if (carSpeed >= 15f)
                 {
                     //TODO: LIGHTS!
-                    carSpeed -= 0.3f;//deceleration
+
+                    //deceleration
+                    carSpeed -= .05f;
+                    //tilt
                     this.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(10, 0, 0), 10 * Time.deltaTime);
                 }
             }
-            buttonPressed = false;
         }
 
     }
 
+    //acceleration
+    public void AccelerationPressed()
+    {
+        accelerationPressed = true;
+    }
+    public void AccelerationReleased()
+    {
+        accelerationPressed = false;
+    }
+
+    //brake
+    public void BrakePressed()
+    {
+        brakePressed = true;
+    }
+    public void BrakeReleased()
+    {
+        brakePressed = false;
+    }
     public void Death()
     {
         //add a pop-up with score etc. ...
