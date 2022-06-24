783d701e659890f479ce0a0b904961f0beac0b79
Adrian-Mihai Onofrei <woozydeveloper@gmail.com>
2022-06-25 02:05:23 +0300

    .

---
+++ Library/Il2cppBuildCache/Android/arm64-v8a/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,287 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void BlinkerScript::Start()
+extern void BlinkerScript_Start_mB7EA5435548B9DC5AB4185B9E0524AC3D180FF9D (void);
+// 0x00000002 System.Void BlinkerScript::Update()
+extern void BlinkerScript_Update_m2FD95390E91173548828BB5EBC8F14593A19E386 (void);
+// 0x00000003 System.Void BlinkerScript::stopBlinking()
+extern void BlinkerScript_stopBlinking_m04530D34E7E3E84BB25275261679494AC1AC6094 (void);
+// 0x00000004 System.Void BlinkerScript::changeState()
+extern void BlinkerScript_changeState_m1E3186942093746B55627F91477ECA953F5EB8A1 (void);
+// 0x00000005 System.Void BlinkerScript::.ctor()
+extern void BlinkerScript__ctor_m7A7545B2628E006CEFDA67F9D7708521614F97F7 (void);
+// 0x00000006 System.Void CameraScript::Start()
+extern void CameraScript_Start_mFA9D3EAD61A387624F2D8E2AB938D32CB43C2AAD (void);
+// 0x00000007 System.Void CameraScript::Update()
+extern void CameraScript_Update_mFD3343EC9B5F59B8D44E7151FA49C7CE88B1936A (void);
+// 0x00000008 System.Void CameraScript::.ctor()
+extern void CameraScript__ctor_m2F555CA91AB185743DD59C5C093A7F6EE339B863 (void);
+// 0x00000009 System.Void CarMovement::Start()
+extern void CarMovement_Start_mC4981073D714CF4DC331ADAA06EA2CD34F1F3419 (void);
+// 0x0000000A System.Void CarMovement::Update()
+extern void CarMovement_Update_m5BADDEA1EA3B999E9877DC8A8FC518609BC4793B (void);
+// 0x0000000B System.Void CarMovement::AccelerationPressed()
+extern void CarMovement_AccelerationPressed_m2FA8660B82193847DB64F982416769D203743A78 (void);
+// 0x0000000C System.Void CarMovement::AccelerationReleased()
+extern void CarMovement_AccelerationReleased_mA7E2802844DCE099ECA9EC9AEB788A0ACFC7719F (void);
+// 0x0000000D System.Void CarMovement::BrakePressed()
+extern void CarMovement_BrakePressed_mCE0944060E3EE272EDED0F85CEA70E03B9AEF08A (void);
+// 0x0000000E System.Void CarMovement::BrakeReleased()
+extern void CarMovement_BrakeReleased_m910A996BD475F9F0B90863BB78E6A7DA8931B1BA (void);
+// 0x0000000F System.Void CarMovement::LeftPressed()
+extern void CarMovement_LeftPressed_m425D5CBD3DBACBBC75C473903537C1D6E9E5C46D (void);
+// 0x00000010 System.Void CarMovement::LeftReleased()
+extern void CarMovement_LeftReleased_m4B5AA6FB3ADFF7018A5B0B160193745DEBB46BA6 (void);
+// 0x00000011 System.Void CarMovement::RightPressed()
+extern void CarMovement_RightPressed_m83131468304DD242B383C3811650EE42B704D642 (void);
+// 0x00000012 System.Void CarMovement::RightReleased()
+extern void CarMovement_RightReleased_mD0972B3B24A7C4F805E58017F3DD4D94B23CEC4A (void);
+// 0x00000013 System.Void CarMovement::Death()
+extern void CarMovement_Death_m12F365D03DFCF9B56B63D38365BC6B687F2894DE (void);
+// 0x00000014 System.Void CarMovement::.ctor()
+extern void CarMovement__ctor_mBD980C544A75DD92006DA60225B760B94DE20A43 (void);
+// 0x00000015 System.Void FPSscript::Update()
+extern void FPSscript_Update_m5A26CBACC8A14F133403A991EAC6A9075AF48269 (void);
+// 0x00000016 System.Void FPSscript::.ctor()
+extern void FPSscript__ctor_m5BF6C5E3DA9EB0E3CE277A40FDFB936505D3919F (void);
+// 0x00000017 System.Void LeftRightLimits::OnCollisionEnter(UnityEngine.Collision)
+extern void LeftRightLimits_OnCollisionEnter_m8EDADA79DCAC2796863F3644A5E3791941073316 (void);
+// 0x00000018 System.Void LeftRightLimits::.ctor()
+extern void LeftRightLimits__ctor_m7F0BF0273EC492CFDB6C713B8CA66825C91C61FB (void);
+// 0x00000019 System.Void MenuScript::Start()
+extern void MenuScript_Start_m124CA94FD1CC7271FBB546327CC3B76E618A5012 (void);
+// 0x0000001A System.Void MenuScript::BackFromGarage()
+extern void MenuScript_BackFromGarage_mB6D3147724B3D4C2108AEC6BB95E1BA5FFA13452 (void);
+// 0x0000001B System.Void MenuScript::GarageGoButton()
+extern void MenuScript_GarageGoButton_m11EB0AB1681F085ECB27156274C18832B93042C8 (void);
+// 0x0000001C System.Void MenuScript::StartButton()
+extern void MenuScript_StartButton_m1C1024FADAAB92E58E88D329CC816B362A54E58E (void);
+// 0x0000001D System.Void MenuScript::OptionsButton()
+extern void MenuScript_OptionsButton_m3AEAEF7D64FFF0B872C3305ADA37010CA4557A40 (void);
+// 0x0000001E System.Void MenuScript::ExitButton()
+extern void MenuScript_ExitButton_m4541EB3E3DC64D5BFA95EE42C9E9D24A507278CC (void);
+// 0x0000001F System.Void MenuScript::Update()
+extern void MenuScript_Update_m364BD9DB8F434326BAF9EC94DE4A1BEF02B93FEB (void);
+// 0x00000020 System.Void MenuScript::ShowButtons(System.Boolean)
+extern void MenuScript_ShowButtons_m55038FBFE3A60133656C2FBF2CE4C1F82D1B7CF8 (void);
+// 0x00000021 System.Void MenuScript::.ctor()
+extern void MenuScript__ctor_m5DE33EE74C32A13B9B00105C42F0E6FC738C3DF6 (void);
+// 0x00000022 System.Void Obstacles::OnCollisionEnter(UnityEngine.Collision)
+extern void Obstacles_OnCollisionEnter_mC510BB30DD25526CB814AF8BB87D1C9E4F1EDCF5 (void);
+// 0x00000023 System.Void Obstacles::.ctor()
+extern void Obstacles__ctor_m9B426B11D90E1B1D1760195F34914F6514A84032 (void);
+// 0x00000024 System.Void OtherCar::Start()
+extern void OtherCar_Start_mD76446C6F081E249C3C44B9D6E5477134ABFA867 (void);
+// 0x00000025 System.Void OtherCar::Update()
+extern void OtherCar_Update_m9C58C30A4A7E869F32930FCDCEF31E30DC073CD0 (void);
+// 0x00000026 System.Int32 OtherCar::Aproximate(System.Single)
+extern void OtherCar_Aproximate_m9E508DDC29CF263B397997DAF7D3B60CDAF9945C (void);
+// 0x00000027 System.Void OtherCar::RespawnCar()
+extern void OtherCar_RespawnCar_m9E5C412D410EE69099561587516ECB345582755C (void);
+// 0x00000028 System.Void OtherCar::ChangeLane()
+extern void OtherCar_ChangeLane_m27CEB9CFF13CE2ACF181481A6BC8E41D362C2B15 (void);
+// 0x00000029 System.Void OtherCar::OnCollisionEnter(UnityEngine.Collision)
+extern void OtherCar_OnCollisionEnter_m6A3289493D3FDDE1EAB947E954AF97F95B580484 (void);
+// 0x0000002A System.Void OtherCar::.ctor()
+extern void OtherCar__ctor_mD0C58F0FD5E45B80359EA1D0FA2043646B024531 (void);
+// 0x0000002B System.Void Sensors::Start()
+extern void Sensors_Start_mC22353996CD86C7857C45F9A2747729815305C14 (void);
+// 0x0000002C System.Void Sensors::OnTriggerEnter(UnityEngine.Collider)
+extern void Sensors_OnTriggerEnter_m0C56FB3749E1ECB9288D58274260B1031E33602B (void);
+// 0x0000002D System.Void Sensors::OnTriggerExit(UnityEngine.Collider)
+extern void Sensors_OnTriggerExit_m9516CC012845B8482C503ABC4540C92D450BB7C6 (void);
+// 0x0000002E System.Void Sensors::.ctor()
+extern void Sensors__ctor_m143F1D3F61838E5B9866F93E28CC27E9E1185DB2 (void);
+// 0x0000002F System.Void Shop::Start()
+extern void Shop_Start_m511166AF24B2A3289A51FD338BFB6E39CD1B1A46 (void);
+// 0x00000030 System.Void Shop::Update()
+extern void Shop_Update_m0BAB0F4160017513DD4CFDA2FEE96BFE06358DB0 (void);
+// 0x00000031 System.Void Shop::RightPress()
+extern void Shop_RightPress_m3B2EA213C78C37E1D33FB7A135E81368CC6B84A4 (void);
+// 0x00000032 System.Void Shop::LeftPress()
+extern void Shop_LeftPress_mF808976348755A05A4887AA62903F2882133B2CE (void);
+// 0x00000033 System.Void Shop::.ctor()
+extern void Shop__ctor_m9321F06993F85B2AE2C6F4E443276AC9D48E738C (void);
+// 0x00000034 System.Void Spawner::Start()
+extern void Spawner_Start_m61D97BD980BD1B1877634A1E7626E47418D5D6D8 (void);
+// 0x00000035 System.Void Spawner::Update()
+extern void Spawner_Update_m8E44DB2210E6C1692B202D38D0867961E9720AA9 (void);
+// 0x00000036 System.Void Spawner::SpawnTerrain()
+extern void Spawner_SpawnTerrain_m42C6F27DAD754AD7C4F9CF6DB7AEB8BF4C4D116C (void);
+// 0x00000037 System.Void Spawner::.ctor()
+extern void Spawner__ctor_m08E8D40AAA40F4329D8A95EEE2B2B6BE842CEB9C (void);
+// 0x00000038 System.Void StartingHouseScript::Start()
+extern void StartingHouseScript_Start_m89CA9CE4298FE9890EE04B174E7C80BA9014001E (void);
+// 0x00000039 System.Void StartingHouseScript::Update()
+extern void StartingHouseScript_Update_m2A988CE2CB70AD269B36125DBBAE6C638542890E (void);
+// 0x0000003A System.Void StartingHouseScript::.ctor()
+extern void StartingHouseScript__ctor_mF13D8E8BAC21173FBECF84AF4DF93A1CBE639EC2 (void);
+// 0x0000003B System.Void WheelScript::Start()
+extern void WheelScript_Start_m7355BCD8C49CC7E8CBF86A566A5F6ABDE1BE0F01 (void);
+// 0x0000003C System.Void WheelScript::Update()
+extern void WheelScript_Update_mBFEEDD022E659EB1B6D1EF19E4161351F0C7F742 (void);
+// 0x0000003D System.Void WheelScript::.ctor()
+extern void WheelScript__ctor_m5DBCC54A10F2D88535759A3F8BCB36B459B9D864 (void);
+static Il2CppMethodPointer s_methodPointers[61] = 
+{
+	BlinkerScript_Start_mB7EA5435548B9DC5AB4185B9E0524AC3D180FF9D,
+	BlinkerScript_Update_m2FD95390E91173548828BB5EBC8F14593A19E386,
+	BlinkerScript_stopBlinking_m04530D34E7E3E84BB25275261679494AC1AC6094,
+	BlinkerScript_changeState_m1E3186942093746B55627F91477ECA953F5EB8A1,
+	BlinkerScript__ctor_m7A7545B2628E006CEFDA67F9D7708521614F97F7,
+	CameraScript_Start_mFA9D3EAD61A387624F2D8E2AB938D32CB43C2AAD,
+	CameraScript_Update_mFD3343EC9B5F59B8D44E7151FA49C7CE88B1936A,
+	CameraScript__ctor_m2F555CA91AB185743DD59C5C093A7F6EE339B863,
+	CarMovement_Start_mC4981073D714CF4DC331ADAA06EA2CD34F1F3419,
+	CarMovement_Update_m5BADDEA1EA3B999E9877DC8A8FC518609BC4793B,
+	CarMovement_AccelerationPressed_m2FA8660B82193847DB64F982416769D203743A78,
+	CarMovement_AccelerationReleased_mA7E2802844DCE099ECA9EC9AEB788A0ACFC7719F,
+	CarMovement_BrakePressed_mCE0944060E3EE272EDED0F85CEA70E03B9AEF08A,
+	CarMovement_BrakeReleased_m910A996BD475F9F0B90863BB78E6A7DA8931B1BA,
+	CarMovement_LeftPressed_m425D5CBD3DBACBBC75C473903537C1D6E9E5C46D,
+	CarMovement_LeftReleased_m4B5AA6FB3ADFF7018A5B0B160193745DEBB46BA6,
+	CarMovement_RightPressed_m83131468304DD242B383C3811650EE42B704D642,
+	CarMovement_RightReleased_mD0972B3B24A7C4F805E58017F3DD4D94B23CEC4A,
+	CarMovement_Death_m12F365D03DFCF9B56B63D38365BC6B687F2894DE,
+	CarMovement__ctor_mBD980C544A75DD92006DA60225B760B94DE20A43,
+	FPSscript_Update_m5A26CBACC8A14F133403A991EAC6A9075AF48269,
+	FPSscript__ctor_m5BF6C5E3DA9EB0E3CE277A40FDFB936505D3919F,
+	LeftRightLimits_OnCollisionEnter_m8EDADA79DCAC2796863F3644A5E3791941073316,
+	LeftRightLimits__ctor_m7F0BF0273EC492CFDB6C713B8CA66825C91C61FB,
+	MenuScript_Start_m124CA94FD1CC7271FBB546327CC3B76E618A5012,
+	MenuScript_BackFromGarage_mB6D3147724B3D4C2108AEC6BB95E1BA5FFA13452,
+	MenuScript_GarageGoButton_m11EB0AB1681F085ECB27156274C18832B93042C8,
+	MenuScript_StartButton_m1C1024FADAAB92E58E88D329CC816B362A54E58E,
+	MenuScript_OptionsButton_m3AEAEF7D64FFF0B872C3305ADA37010CA4557A40,
+	MenuScript_ExitButton_m4541EB3E3DC64D5BFA95EE42C9E9D24A507278CC,
+	MenuScript_Update_m364BD9DB8F434326BAF9EC94DE4A1BEF02B93FEB,
+	MenuScript_ShowButtons_m55038FBFE3A60133656C2FBF2CE4C1F82D1B7CF8,
+	MenuScript__ctor_m5DE33EE74C32A13B9B00105C42F0E6FC738C3DF6,
+	Obstacles_OnCollisionEnter_mC510BB30DD25526CB814AF8BB87D1C9E4F1EDCF5,
+	Obstacles__ctor_m9B426B11D90E1B1D1760195F34914F6514A84032,
+	OtherCar_Start_mD76446C6F081E249C3C44B9D6E5477134ABFA867,
+	OtherCar_Update_m9C58C30A4A7E869F32930FCDCEF31E30DC073CD0,
+	OtherCar_Aproximate_m9E508DDC29CF263B397997DAF7D3B60CDAF9945C,
+	OtherCar_RespawnCar_m9E5C412D410EE69099561587516ECB345582755C,
+	OtherCar_ChangeLane_m27CEB9CFF13CE2ACF181481A6BC8E41D362C2B15,
+	OtherCar_OnCollisionEnter_m6A3289493D3FDDE1EAB947E954AF97F95B580484,
+	OtherCar__ctor_mD0C58F0FD5E45B80359EA1D0FA2043646B024531,
+	Sensors_Start_mC22353996CD86C7857C45F9A2747729815305C14,
+	Sensors_OnTriggerEnter_m0C56FB3749E1ECB9288D58274260B1031E33602B,
+	Sensors_OnTriggerExit_m9516CC012845B8482C503ABC4540C92D450BB7C6,
+	Sensors__ctor_m143F1D3F61838E5B9866F93E28CC27E9E1185DB2,
+	Shop_Start_m511166AF24B2A3289A51FD338BFB6E39CD1B1A46,
+	Shop_Update_m0BAB0F4160017513DD4CFDA2FEE96BFE06358DB0,
+	Shop_RightPress_m3B2EA213C78C37E1D33FB7A135E81368CC6B84A4,
+	Shop_LeftPress_mF808976348755A05A4887AA62903F2882133B2CE,
+	Shop__ctor_m9321F06993F85B2AE2C6F4E443276AC9D48E738C,
+	Spawner_Start_m61D97BD980BD1B1877634A1E7626E47418D5D6D8,
+	Spawner_Update_m8E44DB2210E6C1692B202D38D0867961E9720AA9,
+	Spawner_SpawnTerrain_m42C6F27DAD754AD7C4F9CF6DB7AEB8BF4C4D116C,
+	Spawner__ctor_m08E8D40AAA40F4329D8A95EEE2B2B6BE842CEB9C,
+	StartingHouseScript_Start_m89CA9CE4298FE9890EE04B174E7C80BA9014001E,
+	StartingHouseScript_Update_m2A988CE2CB70AD269B36125DBBAE6C638542890E,
+	StartingHouseScript__ctor_mF13D8E8BAC21173FBECF84AF4DF93A1CBE639EC2,
+	WheelScript_Start_m7355BCD8C49CC7E8CBF86A566A5F6ABDE1BE0F01,
+	WheelScript_Update_mBFEEDD022E659EB1B6D1EF19E4161351F0C7F742,
+	WheelScript__ctor_m5DBCC54A10F2D88535759A3F8BCB36B459B9D864,
+};
+static const int32_t s_InvokerIndices[61] = 
+{
+	1361,
+	1361,
+	1361,
+	1361,
+	1361,
+	1361,
+	1361,
+	1361,
+	1361,
+	1361,
+	1361,
+	1361,
+	1361,
+	1361,
+	1361,
+	1361,
+	1361,
+	1361,
+	1361,
+	1361,
+	1361,
+	1361,
+	1124,
+	1361,
+	1361,
+	1361,
+	1361,
+	1361,
+	1361,
+	1361,
+	1361,
+	1142,
+	1361,
+	1124,
+	1361,
+	1361,
+	1361,
+	813,
+	1361,
+	1361,
+	1124,
+	1361,
+	1361,
+	1124,
+	1124,
+	1361,
+	1361,
+	1361,
+	1361,
+	1361,
+	1361,
+	1361,
+	1361,
+	1361,
+	1361,
+	1361,
+	1361,
+	1361,
+	1361,
+	1361,
+	1361,
+};
+extern const CustomAttributesCacheGenerator g_AssemblyU2DCSharp_AttributeGenerators[];
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	61,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	g_AssemblyU2DCSharp_AttributeGenerators,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
